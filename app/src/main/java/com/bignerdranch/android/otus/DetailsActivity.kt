c2f06407cfcf34d851a7654a06925cff3ac328f8
Kate <catherine-eles@mail.ru>
2021-05-12 03:19:16 +0300

    send invite message

--- app/src/main/java/com/bignerdranch/android/otus/DetailsActivity.kt
+++ app/src/main/java/com/bignerdranch/android/otus/DetailsActivity.kt
@@ -1,9 +1,14 @@
 package com.bignerdranch.android.otus
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class DetailsActivity: AppCompatActivity() {
     companion object {
@@ -13,10 +18,27 @@ class DetailsActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
 
-        intent.getParcelableExtra<FilmData>(FILM_DATA)?.let {
-            findViewById<TextView>(R.id.titleView).text = getString(it.name)
-            findViewById<TextView>(R.id.descriptionView).text = getString(it.description)
-            findViewById<ImageView>(R.id.imageView).setImageResource(it.image)
+        intent.getParcelableExtra<FilmData>(FILM_DATA)?.let { film ->
+            findViewById<TextView>(R.id.titleView).text = getString(film.name)
+            findViewById<TextView>(R.id.descriptionView).text = getString(film.description)
+            findViewById<ImageView>(R.id.imageView).setImageResource(film.image)
+
+            findViewById<Button>(R.id.inviteButton).setOnClickListener {
+                val intent = Intent(Intent.ACTION_SEND).apply {
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, "Интересный фильм, советую посмотреть: ${getString(film.name)}.")
+                }
+
+                try {
+                    startActivity(Intent.createChooser(intent, "Send message with..."))
+                } catch (ex: ActivityNotFoundException) {
+                    Toast.makeText(
+                        this,
+                        "There are no suitable clients installed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
         }
     }
 }
